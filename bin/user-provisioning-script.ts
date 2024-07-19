413ee6cf12a4d4e27f07732ddeef1a4791629f34
SolemnDave <davidrjr94@gmail.com>
2024-07-19 14:45:42 -0400

    Clean up repository for new Python CDK setup

--- bin/user-provisioning-script.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { UserProvisioningScriptStack } from "../lib/user-provisioning-script-stack";
-
-const app = new cdk.App();
-new UserProvisioningScriptStack(app, "UserProvisioningScriptStack");
